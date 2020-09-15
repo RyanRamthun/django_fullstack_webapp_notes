042d69197ebe3776ff8a4cfdaca740c0c8b864ea
RRamthun <70039874+RyanRamthun@users.noreply.github.com>
2020-09-15 15:26:33 -0600

    Update settings.py

--- django_notes/settings.py
+++ django_notes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wk&g#e@x*prq4x2m!y2fw@go2)$0tsq7j=7c8swk(x7tho5pnv'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
